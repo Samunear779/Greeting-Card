864485e21c173cd9799b6f2a3116e48c0453b50e
samunear779 <samunear779@ed.amdsb.ca>
2016-09-26 13:33:49 -0400

    Added starts and lines

--- Greeting Card/Form1.Designer.cs
+++ Greeting Card/Form1.Designer.cs
@@ -28,20 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cardTitle = new System.Windows.Forms.Label();
+            this.happyImage = new System.Windows.Forms.PictureBox();
+            this.funnyTitle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.happyImage)).BeginInit();
             this.SuspendLayout();
+            // 
+            // cardTitle
+            // 
+            this.cardTitle.AutoSize = true;
+            this.cardTitle.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cardTitle.ForeColor = System.Drawing.Color.White;
+            this.cardTitle.Location = new System.Drawing.Point(24, 62);
+            this.cardTitle.Name = "cardTitle";
+            this.cardTitle.Size = new System.Drawing.Size(319, 40);
+            this.cardTitle.TabIndex = 0;
+            this.cardTitle.Text = "Happy Greeting Card";
+            // 
+            // happyImage
+            // 
+            this.happyImage.Image = global::Greeting_Card.Properties.Resources._7;
+            this.happyImage.Location = new System.Drawing.Point(114, 154);
+            this.happyImage.Name = "happyImage";
+            this.happyImage.Size = new System.Drawing.Size(138, 166);
+            this.happyImage.TabIndex = 1;
+            this.happyImage.TabStop = false;
+            // 
+            // funnyTitle
+            // 
+            this.funnyTitle.AutoSize = true;
+            this.funnyTitle.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.funnyTitle.ForeColor = System.Drawing.Color.White;
+            this.funnyTitle.Location = new System.Drawing.Point(-6, 346);
+            this.funnyTitle.Name = "funnyTitle";
+            this.funnyTitle.Size = new System.Drawing.Size(379, 36);
+            this.funnyTitle.TabIndex = 2;
+            this.funnyTitle.Text = "Someone Actually Likes You";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkRed;
             this.ClientSize = new System.Drawing.Size(371, 469);
+            this.Controls.Add(this.funnyTitle);
+            this.Controls.Add(this.happyImage);
+            this.Controls.Add(this.cardTitle);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.happyImage)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label cardTitle;
+        private System.Windows.Forms.PictureBox happyImage;
+        private System.Windows.Forms.Label funnyTitle;
     }
 }
 
--- Greeting Card/Form1.cs
+++ Greeting Card/Form1.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Greeting_Card
@@ -15,6 +15,65 @@ namespace Greeting_Card
         public Form1()
         {
             InitializeComponent();
+          
+
+            
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Graphics fg = this.CreateGraphics();                //Set up graphics pen,brushs,and brushs for later use 
+            SolidBrush whiteBrush = new SolidBrush(Color.White);
+            Pen WhitePen = new Pen(Color.White, 10);
+            Pen YellowPen = new Pen(Color.Yellow, 5);
+            Font smallFont = new Font("Times New Roman", 5);
+            Font drawFont = new Font("Times New Roman", 30);
+            SolidBrush YellowBrush = new SolidBrush(Color.Yellow);
+
+            cardTitle.Visible = false;
+            funnyTitle.Visible = false;
+            happyImage.Visible = false;
+            fg.Clear(Color.Black);
+            Thread.Sleep(500);
+
+            fg.DrawString("Hello", smallFont, whiteBrush, 100, 400);
+
+            fg.FillRectangle(YellowBrush, 247, 91, 5, 5);
+            Thread.Sleep(500);
+            fg.DrawLine(YellowPen, 247, 91, 227, 81);
+
+            fg.FillRectangle(YellowBrush, 227, 81, 5, 5);
+            Thread.Sleep(500);
+            fg.DrawLine(YellowPen, 227, 81, 200, 120);
+
+            fg.FillRectangle(YellowBrush, 200, 120,5, 5);
+            Thread.Sleep(500);
+            fg.DrawLine(YellowPen, 200, 120, 206, 147);
+
+            fg.FillRectangle(YellowBrush, 206, 147,5, 5);
+            Thread.Sleep(500);
+            fg.DrawLine(YellowPen, 206, 147, 118, 182);
+
+            fg.FillRectangle(YellowBrush, 118, 182,5, 5);
+            Thread.Sleep(500);
+            fg.DrawLine(YellowPen, 118, 182, 77, 247);   //stopped here
+
+            fg.FillRectangle(YellowBrush, 77, 247, 5, 5);
+            Thread.Sleep(500);
+            fg.DrawLine(YellowPen, 247, 91, 227, 81);
+
+            fg.FillRectangle(YellowBrush, 133, 218,5, 5);
+            Thread.Sleep(500);
+            fg.DrawLine(YellowPen, 247, 91, 227, 81);
+
+            fg.FillRectangle(YellowBrush, 250, 191,5, 5);
+            Thread.Sleep(500);
+            fg.DrawLine(YellowPen, 247, 91, 227, 81);
+
+            fg.FillRectangle(YellowBrush, 236, 158,5, 5);
+            Thread.Sleep(500);
+            fg.DrawLine(YellowPen, 247, 91, 227, 81);
+
         }
     }
 }
